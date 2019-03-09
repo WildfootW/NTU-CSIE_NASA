83244c105671844a37e389adc9038e5b60164caa
WildfootW <wild.foot.yee.tzwu@gmail.com>
2019-03-09 19:46:46 +0800

    init csie analytics

---
+++ homework-01/CSIE Analytics/csie_analytics.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/WildfootW
+#   Copyright (C) 2019 WildfootW All rights reserved.
+#
+
+# Absolute path to this script, e.g. /home/user/Pwngdb/install.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/Pwngdb
+SCRIPTPATH=$(dirname "$SCRIPT")
+
