24b195675f73b39b8af1332d84255ef07aa76d90
WildfootW <wild.foot.yee.tzwu@gmail.com>
2019-03-10 11:22:15 +0800

    add option parser for bash

--- homework-01/CSIE Analytics/csie_analytics.sh
+++ homework-01/CSIE Analytics/csie_analytics.sh
@@ -10,3 +10,33 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/Pwngdb
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+while [[ ! $# -eq 0 ]]; do
+    key="$1"
+    shift
+    case $key in
+        -a|--option-a)
+            OPTIONA="$1"
+            shift
+            ;;
+        -b|--option-b)
+            OPTIONB="$1"
+            shift
+            ;;
+        -c|--option-c)
+            OPTIONC="$1"
+            shift
+            ;;
+        *)
+            if [[ $FILE != "" ]]; then
+                echo "unknown option \"$FILE\""
+                exit 1
+            fi
+            FILE="$key"
+            ;;
+    esac
+done
+echo "OPTIONA = ${OPTIONA}"
+echo "OPTIONB = ${OPTIONB}"
+echo "OPTIONC = ${OPTIONC}"
+echo "FILE    = ${FILE}"
+
